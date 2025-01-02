c925b95913b396d0e0b10eb90b6de558eadb962e
Diar Ghaderi <diar.ghaderi@gmail.com>
2025-01-03 00:52:27 +0200

    Add env variable names for intelliJ

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_APP_TITLE: string
+    readonly VITE_WALTTI_CLIENT_ID: number
+    readonly VITE_WALTTI_CLIENT_SECRET: string
+    // more env variables...
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv
+  }
